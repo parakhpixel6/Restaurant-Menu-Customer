b79d8c6674a55323fbea6a91c9ce6d476e8d9535
parakh.pixel6 <parakh@pixel6.co>
2023-06-21 19:14:49 +0530

    First Commit Of Customer Side

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    speed: 10,
+    // onclick: {
+    //     init: function() {
+    //       var slides = this.slides;
+    //       //slides[0].classList.add('rotated'); // Add 'rotated' class to the first slide
+    //     },
+    //     slideChange: function() {
+    //       var slides = this.slides;
+    //       var activeIndex = this.activeIndex;
+    //       // Check if the active slide is the first slide
+    //         // Add 'rotated' class to the first slide
+    //         slides[0].classList.add('rotated');
+    
+    //     }
+    //   },
+    //spaceBetween: 30,
+    keyboard: {
+      enabled: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});

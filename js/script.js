75c8bb1b99a93530dfe80f49bb6fcc0d0aa0f1c0
parakh.pixel6 <parakh@pixel6.co>
2023-06-23 19:03:53 +0530

    Evening Commit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,6 @@
 const swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
     speed: 10,
-    // onclick: {
-    //     init: function() {
-    //       var slides = this.slides;
-    //       //slides[0].classList.add('rotated'); // Add 'rotated' class to the first slide
-    //     },
-    //     slideChange: function() {
-    //       var slides = this.slides;
-    //       var activeIndex = this.activeIndex;
-    //       // Check if the active slide is the first slide
-    //         // Add 'rotated' class to the first slide
-    //         slides[0].classList.add('rotated');
-    
-    //     }
-    //   },
     spaceBetween: 100,
     keyboard: {
       enabled: true,
